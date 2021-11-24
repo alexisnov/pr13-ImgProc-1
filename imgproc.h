28aea7c82be0d8743d3f55dbabe532648bf10445
alexisnov <novosselski@gmail.com>
2021-11-24 11:32:46 +0300

    +

---
+++ imgproc.h
@@ -0,0 +1,21 @@
+#ifndef IMGPROC_H
+#define IMGPROC_H
+
+#include <QThread>
+#include <QImage>
+
+class ImgProc: public QThread
+{
+    Q_OBJECT
+public:
+    explicit ImgProc(QString name, QString path);
+    void run() override;
+signals:
+    void ready(QString name);//Готовность изображения
+private:
+    QString path;//Путь к картинке
+    QString name;//Название потока
+    QImage image;//Картинка
+};
+
+#endif // IMGPROC_H
